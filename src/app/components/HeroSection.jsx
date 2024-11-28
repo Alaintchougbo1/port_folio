b398862e975b4cd32e6ad38b881eaeab70508638
alain.tchougbo <alain.tchougbo@epitech.eu>
2024-11-28 19:46:42 +0100

    Suppression du p dans Hero section

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -34,9 +34,6 @@ const HeroSection = () => {
               repeat={Infinity}
             />
           </h1>
-          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-            Passionné par l&apos;innovation technologique, je suis un développeur adaptable, capable de maîtriser rapidement toute technologie pour relever des défis en développement full stack et contribuer à des projets à fort impact.
-          </p>
           <div>
             <Link
               href="/#contact"
